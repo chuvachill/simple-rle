5c34b2f9129cc20ff7c859e28517bca55f32538b
Evgeny <116568732+chuvachill@users.noreply.github.com>
2025-03-16 11:23:22 +0500

    Add compression algorithm

--- src/main.c
+++ src/main.c
@@ -1,85 +1,209 @@
-#include <stdbool.h>
+/* https://en.wikipedia.org/wiki/Run-length_encoding */
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <unistd.h>
+
+#define SEQSIZE 128  // SIZE OF (UN)COMPRESSED SEQUENCE
 #define FILENAME 256
 #define COLOR_GREEN "\033[32m"
 #define COLOR_RED   "\033[31m"
 #define COLOR_RESET "\033[0m"
 
-int compress(FILE *, FILE *);
+void change_extension(char *, size_t, const char *, const char *);
+
+int compress(const char *);
 int add_header(FILE *);
 int add_checksum(FILE *);
 
 int decompress();
 
-
-int read_binary(FILE *);
-void change_extension(char *, size_t, const char *, const char *);
-
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        printf("Usage: %s FILE...\n", argv[0]);
+        printf("Usage: %s [OPTION] FILE...\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    for (int i = 1; i < argc; i++) {
-        /* OPEN INPUT FILE */
-        FILE *in = fopen(argv[i], "rb");
-        if (in == NULL) {
-            fprintf(stderr, "%s: No such file\n", argv[i]);            
-            return EXIT_FAILURE;
-        }
-
-        /* CREATE AND OPEN OUTPUT FILE */
-        char new_filename[FILENAME];
-        change_extension(new_filename, FILENAME, argv[i], ".rle");
-
-        FILE *out = fopen(new_filename, "wb");
-        if (out == NULL) {
-            fprintf(stderr, "Error occured while compressing\n");
-            return EXIT_FAILURE;
-        }
-    
-        /* READ INPUT FILE BYTE BY BYTE */
-        read_binary(in);
-        
-        fclose(in);
-    }
+    for (int i = 1; i < argc; i++)
+        compress(argv[i]);
     
     return EXIT_SUCCESS;
 }
 
-int read_binary(FILE *file) {
-    bool isrepeated = false;
+int compress(const char* ifname) {
+    /* OPEN INPUT FILE */
+    FILE *in = fopen(ifname, "rb");
+    if (in == NULL) {
+        fprintf(stderr, "%s: No such file\n", ifname);            
+        return EXIT_FAILURE;
+    }
     
-    unsigned char byte, prevb;
-
-    unsigned int count = 1;
-    while (fread(&byte, sizeof(byte), 1, file)) {
-        switch (isrepeated) {
-        case false:
-            if (byte == prevb) {
-                isrepeated = true;
-                ++count;
-            } else printf("%02x ", byte);
-            break;
-        case true:
-            if (byte != prevb) {
-                isrepeated = false;
-                printf(COLOR_RED "%u" COLOR_GREEN "%02x" COLOR_RESET " ", count, prevb);
-                printf("%02x ", byte);
-                count = 1;
-            } else ++count;
-            break;
-        }
-        prevb = byte;
+    /* CHANGE OUTPUT FILE EXTENSION */
+    char ofname[FILENAME];
+    change_extension(ofname, FILENAME, ifname, ".rle");
+    
+    /* OPEN OUTPUT FILE */
+    FILE *out = fopen(ofname, "wb");
+    if (out == NULL) {
+        fprintf(stderr, "%s: No such file\n", ofname);            
+        return EXIT_FAILURE;
     }
 
-    if (ferror(file)) {
-        perror("Error reading file\n");
-        return EXIT_FAILURE;
+    unsigned char bytes[SEQSIZE];            // SEQUENCE OF BYTES
+    unsigned char* ptr_byte;                 // ONE BYTE OUT OF SEQUENCE
+    signed char count;                       // COUNT (REPEATED|UNIQUE) BYTES WHILE (POSITIVE|NEGATIVE)
+
+    ptr_byte = bytes;   // GET FIRST BYTE
+    count = 0;          // SET TO BE NOT IN ANY SEQUENCE
+
+    // /* READ INPUT FILE BYTE BY BYTE */
+    // while (fread(ptr_byte, sizeof(bytes[0]), 1, in)) {
+    //     if (count > 0) {
+    //         if ((count >= SEQSIZE - 1) || (*ptr_byte != *(ptr_byte - 1))) {
+    //             /* WRITE COMPRESSED */
+    //             system("clear");
+    //             printf("%d ", count);
+    //             printf("%02x ", bytes[0]);
+    //             fflush(stdout);
+    //             getchar();
+    //             system("clear");
+
+    //             /* SHIFT TO THE START */
+    //             bytes[0] = *ptr_byte;
+    //             ptr_byte = bytes;
+    //             count = 0;
+    //         } else {
+    //             ++count;
+    //         }
+    //     } else if (count < 0) {
+    //         if ((abs(count) >= SEQSIZE) || (*ptr_byte == *(ptr_byte - 1))) {
+    //             /* WRITE UNCOMPRESSED */
+    //             system("clear");
+    //             printf("%d\n", count);
+    //             for (size_t i = 0; i < abs(count); i++) {
+    //                 if ((i != 0) && (i % 16 == 0))
+    //                     printf("\n");
+    //                 printf("%02x ", bytes[i]);
+    //             }
+    //             fflush(stdout);
+    //             getchar();
+    //             system("clear");
+                
+    //             /* SHIFT TO THE START */
+    //             bytes[0] = *ptr_byte;
+    //             ptr_byte = bytes;
+    //             count = 0;
+    //         }
+    //         else {
+    //             --count;
+    //         }
+    //     }
+    //     if ((count == 0) && (ptr_byte != bytes)) {
+    //         if (*ptr_byte == *(ptr_byte - 1))
+    //             count = count + 2;
+    //         else
+    //             count = count - 2;
+    //     }
+    //     printf("%d\n", count);
+    //     usleep(50000);
+    //     system("clear");
+        
+    //     for (size_t i = 0; i < abs(count); i++) {
+    //         if ((i != 0) && (i % 16 == 0))
+    //             printf("\n");
+
+    //         if (count > 0)
+    //             printf(COLOR_RED);
+    //         else if (count < 0)
+    //             printf(COLOR_GREEN);
+            
+    //         printf("%02x " COLOR_RESET, bytes[i]);
+    //     }
+    //     fflush(stdout);
+
+    //     ++ptr_byte;  // GO TO THE NEXT BYTE
+    // }
+
+    // if (count > 0) {
+    //     system("clear");
+    //     printf("%d ", count);
+    //     printf("%02x ", bytes[0]);
+    //     fflush(stdout);
+    //     getchar();
+    //     system("clear");
+    // } else if (count < 0) {
+    //     system("clear");
+    //     printf("%d\n", count);
+    //     for (size_t i = 0; i < abs(count); i++) {
+    //         if ((i != 0) && (i % 16 == 0))
+    //             printf("\n");
+    //         printf("%02x ", bytes[i]);
+    //     }
+    //     fflush(stdout);
+    //     getchar();
+    //     system("clear");
+    // }
+    // else if (count == 0) {
+    //     --count;
+    //     system("clear");
+    //     printf("%d ", count);
+    //     printf("%02x ", bytes[0]);
+    //     fflush(stdout);
+    //     getchar();
+    //     system("clear");
+    // }
+
+    /* READ INPUT FILE BYTE BY BYTE */
+    while (fread(ptr_byte, sizeof(bytes[0]), 1, in)) {
+        if (count > 0) {
+            if ((count >= SEQSIZE - 1) || (*ptr_byte != *(ptr_byte - 1))) {
+                /* WRITE COMPRESSED */
+                fwrite(&count, sizeof(count), 1, out);
+                fwrite(bytes, sizeof(bytes[0]), 1, out);
+
+                /* SHIFT TO THE START */
+                bytes[0] = *ptr_byte;
+                ptr_byte = bytes;
+                count = 0;
+            } else {
+                ++count;
+            }
+        } else if (count < 0) {
+            if ((abs(count) >= SEQSIZE) || (*ptr_byte == *(ptr_byte - 1))) {
+                /* WRITE UNCOMPRESSED */
+                fwrite(&count, sizeof(count), 1, out);
+                fwrite(bytes, sizeof(bytes[0]), abs(count), out);
+                
+                /* SHIFT TO THE START */
+                bytes[0] = *ptr_byte;
+                ptr_byte = bytes;
+                count = 0;
+            }
+            else {
+                --count;
+            }
+        }
+        if ((count == 0) && (ptr_byte != bytes)) {
+            if (*ptr_byte == *(ptr_byte - 1))
+                count = count + 2;
+            else
+                count = count - 2;
+        }
+        ++ptr_byte;  // GO TO THE NEXT BYTE
+    }
+
+    if (count > 0) {
+        fwrite(&count, sizeof(count), 1, out);
+        fwrite(bytes, sizeof(bytes[0]), 1, out);
+    } else if (count < 0) {
+        fwrite(&count, sizeof(count), 1, out);
+        fwrite(bytes, sizeof(bytes[0]), abs(count), out);
+    }
+    else if (count == 0) {
+        fwrite(&count, sizeof(count), 1, out);
+        fwrite(bytes, sizeof(bytes[0]), 1, out);
     }
 
     return EXIT_SUCCESS;
