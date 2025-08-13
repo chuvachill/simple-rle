d0ec3c2dd64a36623cd9671656f078f28570133e
Evgeny Mazin <116568732+chuvachill@users.noreply.github.com>
2025-08-13 12:38:32 +0300

    Remove unnecessary comments

--- src/rle.c
+++ src/rle.c
@@ -1,15 +1,14 @@
-
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define SEQSIZE 128  // SIZE OF (UN)COMPRESSED SEQUENCE
+#define SEQSIZE 128 // SIZE OF (UN)COMPRESSED SEQUENCE
 #define FILENAME 256
 
 int compress(const char *);
 void change_extension(char *, size_t, const char *, const char *);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s FILE...\n", argv[0]);
         return EXIT_FAILURE;
@@ -17,35 +16,36 @@ int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; i++)
         compress(argv[i]);
-    
+
     return EXIT_SUCCESS;
 }
 
-int compress(const char* ifname) {
+int compress(const char *ifname) {
     /* OPEN INPUT FILE */
     FILE *in = fopen(ifname, "rb");
     if (in == NULL) {
-        fprintf(stderr, "%s: No such file\n", ifname);            
+        fprintf(stderr, "%s: No such file\n", ifname);
         return EXIT_FAILURE;
     }
-    
+
     /* CHANGE OUTPUT FILE EXTENSION */
     char ofname[FILENAME];
     change_extension(ofname, FILENAME, ifname, ".rle");
-    
+
     /* OPEN OUTPUT FILE */
     FILE *out = fopen(ofname, "wb");
     if (out == NULL) {
-        fprintf(stderr, "%s: No such file\n", ofname);            
+        fprintf(stderr, "%s: No such file\n", ofname);
         return EXIT_FAILURE;
     }
 
-    unsigned char bytes[SEQSIZE];            // SEQUENCE OF BYTES
-    unsigned char* ptr_byte;                 // ONE BYTE OUT OF SEQUENCE
-    signed char count;                       // COUNT (REPEATED|UNIQUE) BYTES WHILE (POSITIVE|NEGATIVE)
+    unsigned char bytes[SEQSIZE]; // SEQUENCE OF BYTES
+    unsigned char *ptr_byte;      // ONE BYTE OUT OF SEQUENCE
+    signed char
+        count; // COUNT (REPEATED|UNIQUE) BYTES WHILE (POSITIVE|NEGATIVE)
 
-    ptr_byte = bytes;   // GET FIRST BYTE
-    count = 0;          // SET TO BE NOT IN ANY SEQUENCE
+    ptr_byte = bytes; // GET FIRST BYTE
+    count = 0;        // SET TO BE NOT IN ANY SEQUENCE
 
     /* READ INPUT FILE BYTE BY BYTE */
     while (fread(ptr_byte, sizeof(bytes[0]), 1, in)) {
@@ -67,13 +67,12 @@ int compress(const char* ifname) {
                 /* WRITE UNCOMPRESSED */
                 fwrite(&count, sizeof(count), 1, out);
                 fwrite(bytes, sizeof(bytes[0]), abs(count), out);
-                
+
                 /* SHIFT TO THE START */
                 bytes[0] = *ptr_byte;
                 ptr_byte = bytes;
                 count = 0;
-            }
-            else {
+            } else {
                 --count;
             }
         }
@@ -83,7 +82,7 @@ int compress(const char* ifname) {
             else
                 count = count - 2;
         }
-        ++ptr_byte;  // GO TO THE NEXT BYTE
+        ++ptr_byte; // GO TO THE NEXT BYTE
     }
 
     /* WRITE LEFT BYTES */
@@ -93,8 +92,7 @@ int compress(const char* ifname) {
     } else if (count < 0) {
         fwrite(&count, sizeof(count), 1, out);
         fwrite(bytes, sizeof(bytes[0]), abs(count), out);
-    }
-    else if (count == 0) {
+    } else if (count == 0) {
         fwrite(&count, sizeof(count), 1, out);
         fwrite(bytes, sizeof(bytes[0]), 1, out);
     }
@@ -102,7 +100,8 @@ int compress(const char* ifname) {
     return EXIT_SUCCESS;
 }
 
-void change_extension(char* dest, size_t maxname, const char* src, const char* ext) {
+void change_extension(char *dest, size_t maxname, const char *src,
+                      const char *ext) {
     strncpy(dest, src, maxname - 1);
     dest[maxname - 1] = '\0';
 
--- src/unrle.c
+++ src/unrle.c
@@ -1,11 +1,9 @@
-/* https://en.wikipedia.org/wiki/Run-length_encoding */
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int decompress(const char*, const char *);
+int decompress(const char *, const char *);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Usage: %s SOURCE ... TARGET\n", argv[0]);
         return EXIT_FAILURE;
@@ -16,18 +14,18 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-int decompress(const char* ifname, const char* ofname) {
+int decompress(const char *ifname, const char *ofname) {
     /* OPEN INPUT FILE */
     FILE *in = fopen(ifname, "rb");
     if (in == NULL) {
-        fprintf(stderr, "%s: No such file\n", ifname);            
+        fprintf(stderr, "%s: No such file\n", ifname);
         return EXIT_FAILURE;
     }
-    
+
     /* OPEN OUTPUT FILE */
     FILE *out = fopen(ofname, "wb");
     if (out == NULL) {
-        fprintf(stderr, "%s: No such file\n", ofname);            
+        fprintf(stderr, "%s: No such file\n", ofname);
         return EXIT_FAILURE;
     }
 
@@ -42,7 +40,7 @@ int decompress(const char* ifname, const char* ofname) {
             fread(&byte, sizeof(byte), 1, in);
             for (int i = count; i > 0; --i)
                 fwrite(&byte, sizeof(byte), 1, out);
-        /* WRITE UNCOMPRESSED SEQUENCE */
+            /* WRITE UNCOMPRESSED SEQUENCE */
         } else if (count < 0) {
             for (int i = count; i < 0; ++i) {
                 fread(&byte, sizeof(byte), 1, in);
